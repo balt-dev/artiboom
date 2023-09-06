885fd0a7352f623acb990e00d7eb8656ee438c6f
baltdev <heptor42@gmail.com>
2023-09-06 18:53:39 -0500

    Build

--- artiboomCoreModule.cs
+++ artiboomCoreModule.cs
@@ -97,6 +97,7 @@ namespace Celeste.Mod.artiboom
             if (self.Dashes == 0 && self.Inventory.Dashes == 0)
                 idx = 1;
             if (idx >= 0) {
+                Logger.Log(nameof(ArtiboomModule), $"{SaveData.Instance.Assists.PlayAsBadeline}");
                 if (SaveData.Instance.Assists.PlayAsBadeline) {
                     self.Hair.Color = BadelineDashColors[idx];
                 } else {
