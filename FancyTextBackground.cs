02a1f3897a2eb201e18c103164f4df34f6b085d4
baltdev <heptor42@gmail.com>
2023-11-14 18:57:45 -0600

    Build

--- FancyTextBackground.cs
+++ FancyTextBackground.cs
@@ -34,7 +34,7 @@ namespace Celeste.Mod.artiboom {
                     List<FancyText.Node> nodes = group.Nodes;
                     switch(s){
                         case "artiboom_textbox":
-                            Logger.Log(LogLevel.Info, nameof(ArtiboomModule), "Found textbox modifier!");
+                            Logger.Log(LogLevel.Info, nameof(ArtiboomModule), "\n\n\n\n\n\n\n\n\nFound textbox modifier!\n\n\n\n\n\n\n\n\n");
                             nodes.Add(new TextboxChanger(vals.FirstOrDefault() ?? "default"));
                             break;
                     }
