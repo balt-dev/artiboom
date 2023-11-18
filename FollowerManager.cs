709d11314ef3b363f846f346b1365ac1ea5a2fa4
baltdev <heptor42@gmail.com>
2023-11-18 11:08:09 -0600

    Build

--- FollowerManager.cs
+++ FollowerManager.cs
@@ -9,6 +9,7 @@ using System.Linq;
 internal class FollowerManager
 {
 	private Sofanthiel follower;
+	private CommunicationMark mark;
 
 	private bool wasActiveOnLastFrame = false;
 	private bool pastMirror = false;
@@ -16,19 +17,19 @@ internal class FollowerManager
 	private bool CheckPastMirror(Level level) {
 		var area = level.Session.Area;
 		var campaign = area.GetLevelSet();
-		Logger.Log(LogLevel.Info, nameof(ArtiboomModule), $"In campaign {campaign}");
+		Logger.Log(nameof(ArtiboomModule), $"In campaign {campaign}");
 		if (campaign != "Celeste") {
 			return true;
 		}
 		var chapter = area.ChapterIndex;
-		Logger.Log(LogLevel.Info, nameof(ArtiboomModule), $"In chapter {chapter}");
+		Logger.Log(nameof(ArtiboomModule), $"In chapter {chapter}");
 		if (chapter != 5) {
 			return chapter < 5;
 		}
 		var side = area.Mode;
-		Logger.Log(LogLevel.Info, nameof(ArtiboomModule), $"In side {side}");
+		Logger.Log(nameof(ArtiboomModule), $"In side {side}");
 		var room = level.Session.LevelData.Name;
-		Logger.Log(LogLevel.Info, nameof(ArtiboomModule), $"In room {room}");
+		Logger.Log(nameof(ArtiboomModule), $"In room {room}");
         return side switch {
             AreaMode.CSide => false,
             AreaMode.Normal => room[0] == 'c' || room[0] == 'd' || room[0] == 'e',
@@ -39,7 +40,7 @@ internal class FollowerManager
 
 	public void Load()
 	{
-		Logger.Log(LogLevel.Info, nameof(ArtiboomModule), "Loaded Sofanthiel.");
+		Logger.Log(nameof(ArtiboomModule), "Loaded Sofanthiel.");
 		Everest.Events.Level.OnPause += OnPause;
 		On.Celeste.Level.LoadLevel += OnLoadLevel;
 		On.Celeste.Level.UnloadLevel += OnUnloadLevel;
@@ -48,7 +49,7 @@ internal class FollowerManager
 
 	public void UnLoad()
 	{
-		Logger.Log(LogLevel.Info, nameof(ArtiboomModule), "Unloaded Sofanthiel.");
+		Logger.Log(nameof(ArtiboomModule), "Unloaded Sofanthiel.");
 		Everest.Events.Level.OnPause -= OnPause;
 		On.Celeste.Level.LoadLevel -= OnLoadLevel;
 		On.Celeste.Level.UnloadLevel -= OnUnloadLevel;
@@ -59,7 +60,7 @@ internal class FollowerManager
 	{
 		orig.Invoke(self, playerIntro, isFromLoader);
 		Player entity = self.Tracker.GetEntity<Player>();
-		Logger.Log(LogLevel.Info, nameof(ArtiboomModule), "Loading level...");
+		Logger.Log(nameof(ArtiboomModule), "Loading level...");
 		pastMirror = CheckPastMirror(self);
 		if (pastMirror) {
 			Logger.Log(nameof(ArtiboomModule), "Past mirror!");
@@ -68,13 +69,17 @@ internal class FollowerManager
 		{
 			follower = new Sofanthiel(entity.Center + new Vector2(ArtiboomModule.Settings.FollowX * (int)entity.Facing, -ArtiboomModule.Settings.FollowY - 5f));
 			self.Add(follower);
+			if (pastMirror) {
+				mark = new CommunicationMark(entity.Center + new Vector2(0, -ArtiboomModule.Settings.FollowY - 5f));
+				self.Add(mark);
+			}
 		}
 		wasActiveOnLastFrame = ArtiboomModule.Settings.EnableFollower;
 	}
 
 	private void OnUnloadLevel(On.Celeste.Level.orig_UnloadLevel orig, Level self) {
 		orig.Invoke(self);
-		Logger.Log(LogLevel.Info, nameof(ArtiboomModule), "Unloading level...");
+		Logger.Log(nameof(ArtiboomModule), "Unloading level...");
 		wasActiveOnLastFrame = false;
 	}
 
@@ -82,15 +87,24 @@ internal class FollowerManager
 	{
 		orig.Invoke(self);
 		if (Engine.Scene is Level level) {
-			if (!wasActiveOnLastFrame && ArtiboomModule.Settings.EnableFollower && level.Tracker.CountEntities<Sofanthiel>() == 0)
+			if (!wasActiveOnLastFrame && ArtiboomModule.Settings.EnableFollower)
 			{
-				follower = new Sofanthiel(self.Center + new Vector2(ArtiboomModule.Settings.FollowX * (int)self.Facing, -ArtiboomModule.Settings.FollowY - 5f));
-				level.Add(follower);
-				follower.Position = self.Center + new Vector2(ArtiboomModule.Settings.FollowX * (int)self.Facing, -ArtiboomModule.Settings.FollowY - 5f);
+				if (level.Tracker.CountEntities<Sofanthiel>() == 0) {
+					follower = new Sofanthiel(self.Center + new Vector2(ArtiboomModule.Settings.FollowX * (int)self.Facing, -ArtiboomModule.Settings.FollowY - 5f));
+					level.Add(follower);
+				}
+				if (pastMirror && level.Tracker.CountEntities<CommunicationMark>() == 0) {
+					mark = new CommunicationMark(self.Center + new Vector2(0, -ArtiboomModule.Settings.FollowY - 5f));
+					level.Add(mark);
+				}
 			}
-			if (wasActiveOnLastFrame && !ArtiboomModule.Settings.EnableFollower && level.Tracker.CountEntities<Sofanthiel>() > 0)
+			
+			if (wasActiveOnLastFrame && !ArtiboomModule.Settings.EnableFollower)
 			{
-				follower.Disable();
+				if (level.Tracker.CountEntities<Sofanthiel>() > 0)
+					follower.Disable();
+				if (level.Tracker.CountEntities<CommunicationMark>() > 0)
+					mark.Disable();
 			}
 			wasActiveOnLastFrame = ArtiboomModule.Settings.EnableFollower;
 		} else {
@@ -105,10 +119,15 @@ internal class FollowerManager
 		{
 			item.UpdateSettings();
 		}
+		foreach (CommunicationMark item in level.Entities.FindAll<CommunicationMark>())
+		{
+			item.UpdateSettings();
+		}
 	}
 
 	public void OnPause(Level level, int startIndex, bool minimal, bool quickReset)
 	{
 		follower?.UpdateSettings();
+		mark?.UpdateSettings();
 	}
 }
--- artiboomFollower.cs
+++ artiboomFollower.cs
@@ -70,4 +70,69 @@ namespace Celeste.Mod.artiboom.Entities
 
         } 
     }
+
+    // don't care enough to not code duplicate
+    [Tracked]
+    public class CommunicationMark : Entity
+    {
+        public Sprite sprite;
+        public BloomPoint bloom;
+
+        public int followY = ArtiboomModule.Settings.FollowY * 3;
+
+        public CommunicationMark(Vector2 position) : base(position)
+        {
+            UpdateSettings();
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            Player player = Scene.Tracker.GetEntity<Player>();
+
+            if (player != null)
+            {
+                Vector2 position = Position;
+                Vector2 playerPosition = player.Position + new Vector2(0, -followY - 5f);
+                Position = position + (playerPosition - position) * (1f - (float)Math.Pow(0.1, Engine.DeltaTime));
+            }
+        }
+
+        //Floors the sprite position so that the pixels are neat and don't flicker
+        public override void Render()
+        {
+            Vector2 renderPosition = sprite.RenderPosition;
+            sprite.RenderPosition = sprite.RenderPosition.Floor();
+            base.Render();
+            sprite.RenderPosition = renderPosition;
+        }
+
+
+
+        public IEnumerator Collect()
+        {
+            yield return null;
+            RemoveSelf();
+        }
+
+        public void Disable()
+        {
+            Add(new Coroutine(Collect()));
+        }
+
+        public void UpdateSettings()
+        {
+            Remove(sprite);
+            Remove(bloom);
+
+            sprite = GFX.SpriteBank.Create("communication_mark");
+
+            followY = ArtiboomModule.Settings.FollowY * 3;
+
+            Depth = -10;
+            Add(bloom = new BloomPoint(0.3f, 8f));
+            Add(sprite);
+
+        } 
+    }
 }
