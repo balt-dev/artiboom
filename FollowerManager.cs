94f24204a39385bd5da6895f7adbca7a08f07b53
baltdev <heptor42@gmail.com>
2023-11-18 10:31:57 -0600

    Build

--- FollowerManager.cs
+++ FollowerManager.cs
@@ -9,7 +9,7 @@ internal class FollowerManager
 {
 	private Sofanthiel follower;
 
-	private bool wasActiveOnLastFrame;
+	private bool wasActiveOnLastFrame = false;
 
 	private void IsPastMirror() {
 		Logger.Log(LogLevel.Info, nameof(ArtiboomModule), $"\n\n\nAttempting to load\n\n\n");
@@ -26,7 +26,6 @@ internal class FollowerManager
 	{
 		Logger.Log(LogLevel.Info, nameof(ArtiboomModule), "Loaded Sofanthiel.");
 		Everest.Events.Level.OnPause += OnPause;
-		On.Celeste.Level.LoadLevel += OnLoadLevel;
 		On.Celeste.Level.UnloadLevel += OnUnloadLevel;
 		On.Celeste.Player.Update += OnPlayerUpdate;
 	}
@@ -34,21 +33,9 @@ internal class FollowerManager
 	public void UnLoad()
 	{
 		Logger.Log(LogLevel.Info, nameof(ArtiboomModule), "Unloaded Sofanthiel.");
-		Everest.Events.Level.OnPause += OnPause;
-		On.Celeste.Level.LoadLevel -= OnLoadLevel;
+		Everest.Events.Level.OnPause -= OnPause;
+		On.Celeste.Level.UnloadLevel -= OnUnloadLevel;
 		On.Celeste.Player.Update -= OnPlayerUpdate;
-	}
-
-	private void OnLoadLevel(On.Celeste.Level.orig_LoadLevel orig, Level self, Player.IntroTypes playerIntro, bool isFromLoader)
-	{
-		orig.Invoke(self, playerIntro, isFromLoader);
-		Player entity = self.Tracker.GetEntity<Player>();
-		if (ArtiboomModule.Settings.EnableFollower && entity != null)
-		{
-			follower = new Sofanthiel(entity.Center + new Vector2(ArtiboomModule.Settings.FollowX * (int)entity.Facing, -ArtiboomModule.Settings.FollowY - 5f));
-			self.Add(follower);
-		}
-		wasActiveOnLastFrame = ArtiboomModule.Settings.EnableFollower;
 	}
 
 	private void OnUnloadLevel(On.Celeste.Level.orig_UnloadLevel orig, Level self) {
